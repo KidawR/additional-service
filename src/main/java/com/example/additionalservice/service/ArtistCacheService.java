88ef85d51b18a43be079d3488f024ffce139ad33
KidawR <vadimser2002@mail.ru>
2025-04-24 16:01:57 +0300

    Update ArtistCacheService.java

--- src/main/java/com/example/additionalservice/service/ArtistCacheService.java
+++ src/main/java/com/example/additionalservice/service/ArtistCacheService.java
@@ -5,12 +5,20 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ArtistCacheService {
-
     private final Map<Long, Artist> artistCache = new ConcurrentHashMap<>();
 
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+    public ArtistCacheService() {
+        scheduler.scheduleAtFixedRate(() ->
+                System.out.println("[Artist Cache] Current size: " + artistCache.size()), 0, 30, TimeUnit.SECONDS);
+    }
     public Artist getArtist(Long artistId) {
         return artistCache.get(artistId);
     }
