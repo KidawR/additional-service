a1a57d49959a8c7103039d4f9294e01558971b21
KidawR <vadimser2002@mail.ru>
2025-04-25 13:45:33 +0300

    k

--- src/main/java/com/example/additionalservice/AdditionalServiceApplication.java
+++ src/main/java/com/example/additionalservice/AdditionalServiceApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.ResponseEntity;
-
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class AdditionalServiceApplication {
 	public static void main(String[] args) {
